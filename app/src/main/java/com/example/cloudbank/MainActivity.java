f84225366e9bca3d4402c7a7f0c65b96169a37a6
unknown <amine.jh99@gmail.com>
2021-10-25 11:27:06 +0100

    first commit

---
+++ app/src/main/java/com/example/cloudbank/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.cloudbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View v){
+
+
+    }
+
+    }
